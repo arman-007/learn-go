e16b3d258976804313873bb3291a71cd66c8a86c
arman-007 <armanhosen9@gmail.com>
2024-12-18 15:06:17 +0600

    till go struct and interface completed

---
+++ function_with_multiple_return_value.go
@@ -0,0 +1,19 @@
+// package main
+
+// import "fmt"
+
+// func vals() (int, int, int) {
+//     return 3, 7, 9
+// }
+
+// func main() {
+
+//     a, b, z := vals()
+//     fmt.Println(a)
+//     fmt.Println(b)
+//     fmt.Println(z)
+
+//     _, c, d := vals()
+//     fmt.Println(c)
+//     fmt.Println(d)
+// }
